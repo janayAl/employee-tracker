d0992d137c81b6699b00fded68d158322379606a
janayAl <jsallen298705@gmail.com>
2021-07-28 14:25:43 -0500

    updated query with inner joins

--- db/query.sql
+++ db/query.sql
@@ -1,14 +1,28 @@
-SELECT departments.department_name AS department, role.title
-FROM role
-JOIN department
-ON role.role_id = department.id
-ORDER BY department.department_name;
+SELECT department.name, role.title, role.salary, employee.first_name, employee.last_name, employee.manager_id
+FROM department
+INNER JOIN role ON role.role_id = department.id
+INNER JOIN employee ON role.salry = employee.id
+ORDER BY department.name ASC;
 
-SELECT role.role_name AS role, role.title
-FROM role
-LEFT JOIN department
-ON role.role_id = department.id
-ORDER BY department.department_name;
+
+-- SELECT department.department_name AS department, role.title
+-- FROM role
+-- JOIN department
+-- ON role.role_id = department.id
+-- ORDER BY department.department_name;
+
+-- SELECT role.role_name AS role, role.title
+-- FROM role
+-- LEFT JOIN department
+-- ON role.role_id = department.id
+-- ORDER BY department.name;
+
+--SELECT *
+-- FROM role
+-- JOIN department
+-- ON role.department= department.id;
+
+
 
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -3,18 +3,18 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    role_id INT,
+role_id INT,
   title VARCHAR(30),
   salary DECIMAL NOT NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
+    FOREIGN KEY (department)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
@@ -25,7 +25,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
    last_name VARCHAR(30),
     FOREIGN KEY (role_id)
-    REFERENCES roles(id)
+    REFERENCES role(id)
     ON DELETE SET NULL
     manager_id INT
     REFERENCES employee(id)
