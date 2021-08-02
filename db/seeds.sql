a1c422f1e7d1747e26de7af2279968b30b1dbeaa
janayAl <jsallen298705@gmail.com>
2021-08-02 18:30:53 -0500

    seeds update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,16 @@
-INSERT INTO department (id, department_name)
-VALUES (1"Air"),
-       (2,"Water"),
-       (3,"Earth");
+INSERT INTO department (id, name)
+VALUES (1, 'Air'),
+       (2, 'Water'),
+       (3, 'Earth');
 
        INSERT INTO role (role_id, title, salary)
-VALUES (1, "Bird", "$500,000",),
-       (2, "Fish", "$250,000"),
-       (3, "Deer", "100,000");
+VALUES (1, 'Bird', '$500,000',),
+       (2, 'Fish', '$250,000'),
+       (3, 'Deer', '100,000');
        
 
        INSERT INTO employee (employee_id, first_name, last_name, manager_id)
-VALUES (1, "Scuttle", "Shores", 1),
-       (2, "Flounder","Dunger", 2 ),
-       (3, "Bambi", "Bloom", 3),
+VALUES (1, 'Scuttle', 'Shores', 1),
+       (2, 'Flounder','Dunger', 2 ),
+       (3, 'Bambi', 'Bloom', 3);
+    
