5aaf568585169568b644fa275e02ce148759cd14
janayAl <jsallen298705@gmail.com>
2021-08-04 23:59:33 -0500

    switch case for each function to use with inquirer prompts

--- index.js
+++ index.js
@@ -2,60 +2,86 @@ const express = require('express')
 const mysql = require('mysql')
 const mysql2 = require('mysql2')
 const Connection = require('mysql2/typings/mysql/lib/Connection')
-const inquirer = require ('inquirer')
+const inquirer = require('inquirer')
 
-connection.connect(function(error){
-    if(error) throw error;
-    console.log("connected at " +connection.threadId+"\n");
+connection.connect(function (error) {
+    if (error) throw error;
+    console.log("connected at " + connection.threadId + "\n");
     initialQuestions() //add a function to prompt initial questions
 })
+
+function initialQuestions() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'ViewOptions',
+            message: 'What would you like to do?',
+            Choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role',]
+        }
+
+    ])
+        .then(function(answer) {   //function asnswer vs value? 
+            switch (answer.ViewOptions) {   // switches the responses to equal/yield its corresponding function
+                case 'view all departments':
+                    viewDepartment();
+                    break;
+                case 'view all roles':
+                    ViewRole();
+                    break;
+                    case 'view all employees':
+                    ViewEmployee();
+                    break;
+                case 'add a department':
+                    AddDepartment(); //create add department function  call
+                    break;
+                case 'add a role':
+                    AddRole(); //add role funciton call
+                    break;
+                case 'add an emplyee':
+                    AddEmployee(); //add employee function call 
+                    break;
+                case 'update an employee role':
+                    UpdateEmployeeRole(); // update employee function call
+                    break;
+//exit or come back to initial questions? //
+            }
+        })
+    
+}
 //add view department function
 
-function viewrole() {
-    Connection.Query(`SELECT * FROM department`, 
-    function (err, results) {
-        if (err) throw err; // added if statement for throw error if an erorr
-        console.table(results); //console.table displays tabular data as a table
-        startMenu();// add a function to start back from the top when done?
-    });
+function viewDepartment() {
+    Connection.Query(`SELECT * FROM department`,
+        function (err, results) {
+            if (err) throw err; // added if statement for throw error if an erorr
+            console.table(results); //console.table displays tabular data as a table
+            startMenu();// add a function to start back from the top when done?
+        });
 }
 
 
 //add view roles function
-function viewrole() {
+function ViewRole() {
     Connection.Query(`SELECT * FROM role`, function (err, results) {
         if (err) throw err;
         console.table(results);
-        startMenu();
+        initialQuestions();
     });
 }
 
 //View employees function
-function viewemployee() {
+function ViewEmployee() {
     connection.query(`SELECT department.name, role.title, role.salary, employee.first_name, employee.last_name, employee.manager_id as manager
 FROM employee
 LEFT JOIN role ON employee.role_id = role.id
 LEFT JOIN department ON role.department_id = department.id
 LEFT JOIN employee manager on manager.id = employee.manager_id;`, function (err, results) {
-    if (err) throw err;
-    console.table(results);
-    startMenu();
-});
+        if (err) throw err;
+        console.table(results);
+        initialQuestions();
+    });
 }
 
-function initialQuestions() {
-    inquirer.prompt([
-        {
-        type: 'list',
-        name: 'viewOptions',
-        message: 'What would you like to do?', 
-        Choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee',  'update an employee role',]
-        },
-
-    ])
-    .then ()
-        
-}
 
 // const updateEmployeeRole = async () => {
 //   // get employees and choice one
